cde5276313ccc4c68480fb578dffb5a18c231509
Jon Simpson <jon@jonsimpson.ca>
2015-11-18 18:05:17 -0500

    - ServiceIdentifier generates a hashed service identifier according to section 3.1 of the paper - ServiceIdentifier implements Key, for integration with OpenChord - Add Commons Lang and JUnit as dependencies - Test ServiceIdentifier

--- src/main/java/com/chord4js/ServiceIdentifier.java
+++ src/main/java/com/chord4js/ServiceIdentifier.java
@@ -1,10 +1,10 @@
 package com.chord4js;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import de.uniba.wiai.lspi.chord.service.Key;
 import de.uniba.wiai.lspi.chord.service.ServiceException;
@@ -13,14 +13,31 @@ import de.uniba.wiai.lspi.chord.service.ServiceException;
  * A service identifier that provides a functional description of the service
  * and the location (provider) of that service. Quality-of-service information
  * is also kept.
+ * 
+ * <br><br>
+ * We assume that a service identifier has at least 1 and at most
+ * <code>{@link #FUNCTIONAL_LAYERS}</code> worth of functional layers and 1
+ * provider layer.
  */
 public class ServiceIdentifier implements Key {
 	
+	public static final int FUNCTIONAL_LAYERS = 4;
+	public static final int PROVIDER_LAYERS = 1;
 	/**
 	 * Number of layers (functional + provider) that make up a service
 	 * identifier. Used in creating the hashed key.
 	 */
-	public static final int LAYERS = 5;
+	public static final int TOTAL_LAYERS = FUNCTIONAL_LAYERS + PROVIDER_LAYERS;
+	
+	/**
+	 * Length of a SHA-1 hash in hex string form
+	 */
+	static final int SHA1_LENGTH = 40;
+	
+	/**
+	 * The hash to put in place of a layer with no value
+	 */
+	static final String EMPTY_LAYER = StringUtils.repeat('0', SHA1_LENGTH);
 	
 	private List<String> layers = new ArrayList<String>();
 	private List<String> qos = new ArrayList<String>();
@@ -31,6 +48,10 @@ public class ServiceIdentifier implements Key {
 	}
 	
 	public void setLayers(List<String> layers) {
+		if (layers.size() > FUNCTIONAL_LAYERS) {
+			throw new IllegalArgumentException("too many functional layers. Given " + layers.size()
+					+ ", expected " + FUNCTIONAL_LAYERS + " or less");
+		}
 		this.layers = layers;
 	}
 	
@@ -55,9 +76,12 @@ public class ServiceIdentifier implements Key {
 	 * @return
 	 * @throws ServiceException
 	 */
-	public String getHashedIdentifier() throws ServiceException {
-		if (layers.size() == 0) {
-			throw new ServiceException("Not enough functional bits to construct data identifier");
+	public String getHashedIdentifier() {
+		int layerSize = layers.size();
+		int missingLayers = FUNCTIONAL_LAYERS - layerSize;
+		if (layerSize == 0) {
+			throw new IllegalArgumentException(
+					"Not enough functional bits to construct data identifier");
 		}
 		
 		// TODO: assume 5 parts to service identifier, pad sections with zeroes
@@ -69,14 +93,25 @@ public class ServiceIdentifier implements Key {
 		// a human readable sha1
 		StringBuilder builder = new StringBuilder();
 		for (String layer : layers) {
-			builder.append(DigestUtils.sha1Hex(layer));
+			builder.append(hash(layer));
 		}
-		builder.append(DigestUtils.sha1Hex(provider));
+		
+		// fill in zeroes for missing functional layers
+		for (int i = 0; i < missingLayers; i++) {
+			builder.append(EMPTY_LAYER);
+		}
+		
+		builder.append(hash(provider));
 		
 		return builder.toString();
 	}
 	
-	public byte[] getBytes() {
-		return null;
+	public static String hash(String data) {
+		return DigestUtils.sha1Hex(data);
 	}
+	
+	public byte[] getBytes() {
+		return getHashedIdentifier().getBytes();
+	}
+	
 }
