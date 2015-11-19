06e14a6a8f9d5cd0016c2c147669a52a1decdd99
Jon Simpson <jon@jonsimpson.ca>
2015-11-19 18:27:05 -0500

    Update the Service class to only contain a ProviderId and QoS information

--- src/main/java/com/chord4js/Service.java
+++ src/main/java/com/chord4js/Service.java
@@ -1,127 +1,48 @@
 package com.chord4js;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.lang3.StringUtils;
-
-import de.uniba.wiai.lspi.chord.service.Key;
-import de.uniba.wiai.lspi.chord.service.ServiceException;
 
 /**
  * A Service contains a ProviderId with Quality-of-Service information.
- * 
- * <br>
- * <br>
- * We assume that a service has at least 1 and at most
- * <code>{@link #FUNCTIONAL_LAYERS}</code> worth of functional layers and 1
- * provider layer.
  */
-public class Service implements Key {
+public class Service {
 	
 	private final ProviderId providerId;
+	private final List<String> qos;
 	
-	public static final int FUNCTIONAL_LAYERS = 4;
-	public static final int PROVIDER_LAYERS = 1;
 	/**
-	 * Number of layers (functional + provider) that make up a service
-	 * identifier. Used in creating the hashed key.
+	 * Create a Service with the given ProviderId and QoS information.
+	 * 
+	 * @param providerId
+	 * @param qos
 	 */
-	public static final int TOTAL_LAYERS = FUNCTIONAL_LAYERS + PROVIDER_LAYERS;
+	public Service(ProviderId providerId, List<String> qos) {
+		this.providerId = providerId;
+		this.qos = qos;
+		
+	}
 	
 	/**
-	 * Length of a SHA-1 hash in hex string form
-	 */
-	static final int SHA1_LENGTH = 40;
-	
-	/**
-	 * The hash to put in place of a layer with no value
-	 */
-	static final String EMPTY_LAYER = StringUtils.repeat('0', SHA1_LENGTH);
-	
-	private List<String> layers = new ArrayList<String>();
-	private List<String> qos = new ArrayList<String>();
-	private String provider;
-	
-	/**
+	 * Create a Service with the ProviderId and no QoS information.
 	 * 
 	 * @param providerId
 	 */
 	public Service(ProviderId providerId) {
 		this.providerId = providerId;
-		
-	}
-	
-	public void addQos(String qos2) {
-		qos.add(qos2);
-	}
-	
-	public void setLayers(List<String> layers) {
-		if (layers.size() > FUNCTIONAL_LAYERS) {
-			throw new IllegalArgumentException("too many functional layers. Given " + layers.size()
-					+ ", expected " + FUNCTIONAL_LAYERS + " or less");
-		}
-		this.layers = layers;
-	}
-	
-	public void setProvider(String provider) {
-		this.provider = provider;
-	}
-	
-	public String getProvider() {
-		return provider;
-		
+		qos = null;
 	}
 	
 	@Override
 	public String toString() {
-		return layers + "" + provider + "" + qos;
-	}
-	
-	/**
-	 * Get the functional part of this identifier in string form. This
-	 * corresponds to section 3.1 of the paper:
-	 * 
-	 * @return
-	 * @throws ServiceException
-	 */
-	public String getHashedService() {
-		int layerSize = layers.size();
-		int missingLayers = FUNCTIONAL_LAYERS - layerSize;
-		if (layerSize == 0) {
-			throw new IllegalArgumentException(
-					"Not enough functional bits to construct data identifier");
-		}
-		
-		// append all layers to the StringBuilder, hashing them at the same time
-		// according to section 3.1 of the paper. The hashing algorithm outputs
-		// a human readable sha1
-		StringBuilder builder = new StringBuilder();
-		for (String layer : layers) {
-			builder.append(hash(layer));
-		}
-		
-		// fill in zeroes for missing functional layers
-		for (int i = 0; i < missingLayers; i++) {
-			builder.append(EMPTY_LAYER);
-		}
-		
-		builder.append(hash(provider));
-		
-		return builder.toString();
-	}
-	
-	public static String hash(String data) {
-		return DigestUtils.sha1Hex(data);
-	}
-	
-	public byte[] getBytes() {
-		return getHashedService().getBytes();
+		return getProviderId() + " qos:" + getQos();
 	}
 	
 	public ProviderId getProviderId() {
 		return providerId;
+	}
+
+	public List<String> getQos() {
+		return qos;
 	}
 	
 }
